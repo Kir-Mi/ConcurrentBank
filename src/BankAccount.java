50b0cd24cd7321235860e2b1505e8f3d02165eec
kirill <you_can_more@mail.ru>
2024-03-12 10:00:29 +0100

    done

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,29 @@
 public class BankAccount {
+    private final Integer accountNumber;
+    private double balance;
+
+    public BankAccount(Integer accountNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.balance = initialBalance;
+    }
+
+    public synchronized void deposit(double amount) {
+        balance += amount;
+    }
+
+    public synchronized void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+    }
+
+    public synchronized double getBalance() {
+        return balance;
+    }
+
+    public Integer getAccountNumber() {
+        return accountNumber;
+    }
 }
--- src/ConcurrentBank.java
+++ src/ConcurrentBank.java
@@ -1,2 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConcurrentBank {
+    private final Map<Integer, BankAccount> accounts;
+    private Integer accountNumber = 0;
+
+    public ConcurrentBank() {
+        this.accounts = new HashMap<>();
+    }
+
+    public synchronized BankAccount createAccount(double initialBalance) {
+        ++accountNumber;
+        accounts.put(accountNumber, new BankAccount(accountNumber, initialBalance));
+        return accounts.get(accountNumber);
+    }
+
+    public synchronized void transfer(BankAccount fromAccount, BankAccount toAccount, double amount) {
+        if (!accounts.containsValue(fromAccount) || !accounts.containsValue(toAccount)) {
+            throw new IllegalArgumentException("One of the accounts does not exist");
+        }
+
+        synchronized (fromAccount) {
+            synchronized (toAccount) {
+                fromAccount.withdraw(amount);
+                toAccount.deposit(amount);
+            }
+        }
+        System.out.println(String.format("Account %d = %.2f", fromAccount.getAccountNumber(), fromAccount.getBalance()));
+        System.out.println(String.format("Account %d = %.2f", toAccount.getAccountNumber(), toAccount.getBalance()));
+    }
+
+    public synchronized double getTotalBalance() {
+        double totalBalance = 0;
+        for (BankAccount account : accounts.values()) {
+            synchronized (account) {
+                totalBalance += account.getBalance();
+            }
+        }
+        return totalBalance;
+    }
 }
